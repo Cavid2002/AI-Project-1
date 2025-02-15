c71b452e75a66e8448a8156d87aa2562ed41dae3
Cavid2002 <quliyevcavid132@gmail.com>
2025-02-15 18:58:37 +0400

    starting with report

--- GridSearch.java
+++ GridSearch.java
@@ -1,20 +1,22 @@
 import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.PriorityQueue;
-import java.util.Scanner;
 import java.util.Objects;
 
-class Point {
+class Point
+{
     public int x;
     public int y;
 
-    Point(int x, int y) {
+    Point(int x, int y)
+    {
         this.x = x;
         this.y = y;
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o)
+    {
         if (this == o) return true;
         if (!(o instanceof Point)) return false;
         Point point = (Point) o;
@@ -22,7 +24,8 @@ class Point {
     }
 
     @Override
-    public int hashCode() {
+    public int hashCode()
+    {
         return Objects.hash(x, y);
     }
 }
@@ -39,7 +42,8 @@ class Node implements Comparable<Node>
     }
 
     @Override
-    public int compareTo(Node other) {
+    public int compareTo(Node other)
+    {
         return Integer.compare(this.cost, other.cost);
     }
 
@@ -55,7 +59,6 @@ public class GridSearch
     private static final int[] dx = {-1, 1, 0, 0};
     private static final int[] dy = {0, 0, -1, 1};
 
-    /** Reads a grid from a file to the array of 1/0 ints **/
     GridSearch(String filename, int row, int col)
     {   
         this.row = row;
@@ -86,7 +89,6 @@ public class GridSearch
         
     }
 
-
     void print_map()
     {
         for(int i = 0; i < row; i++)
@@ -99,18 +101,61 @@ public class GridSearch
         }
     }
 
-
     public boolean isValid(Point p)
     {
         if(p.x < 0 || p.y < 0 || p.x >= col || p.y >= row) return false;
-        return map[p.y * col + p.x] == 0;
+        return map[p.y * this.col + p.x] == 0;
     }
 
+    public int heuristics(Point p1, Point p2)
+    {
+        int mdist = Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
+        return mdist;
+    }
+
+    public int aStar(Point start, Point stop)
+    {
+        PriorityQueue<Node> pq = new PriorityQueue<>(); 
+        HashMap<Point, Integer> gCostMap = new HashMap<>();
+
+        gCostMap.put(start, 0);
+        pq.add(new Node(start, heuristics(start, stop)));
+
+        while(!pq.isEmpty())
+        {
+            Node current = pq.poll();
+            Point currPoint = current.p;    
+            int gCost = gCostMap.get(currPoint);
+
+            if(currPoint.equals(stop)) return gCost;
+
+            for(int i = 0; i < 4; i++)
+            {
+                Point np = new Point(currPoint.x + dx[i], currPoint.y + dy[i]);
+
+                if(isValid(np) == false) continue;
+                
+                int h = heuristics(np, stop);
+                int g = gCost + 1;
+                int f = g + h;
+                
+                if(!gCostMap.containsKey(np) || gCostMap.get(np) > g)
+                {   
+                    gCostMap.put(np, g);
+                    pq.add(new Node(np, f));
+                }
+
+            }
+            
+        }
+
+        return -1;
+    }
+    
     public int ucs(Point start, Point stop)
     {
         PriorityQueue<Node> pq = new PriorityQueue<>(); 
         HashMap<Point, Integer> costMap = new HashMap<>();
-
 
         pq.add(new Node(start, 0));
         costMap.put(start, 0);
@@ -122,8 +167,6 @@ public class GridSearch
             int currCost = current.cost;
             
             if(currPoint.equals(stop)) return currCost;
-            
-            if(currCost > costMap.get(currPoint)) continue;
 
             for(int i = 0; i < 4; i++)
             {
@@ -144,80 +187,8 @@ public class GridSearch
             
         }
         
-
         return -1;
     }
 
-    public int heuristics(Point p1, Point p2)
-    {
-        int mdist = Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
-        return mdist;
-    }
-
-
-    public int aStart(Point start, Point stop)
-    {
-        PriorityQueue<Node> pq = new PriorityQueue<>(); 
-        HashMap<Point, Integer> costMap = new HashMap<>();
-
-        costMap.put(start, 0);
-        pq.add(new Node(start, heuristics(start, stop)));
-
-        while(!pq.isEmpty())
-        {
-            Node current = pq.poll();
-            Point currPoint = current.p;
-            int currCost = costMap.get(currPoint);
-
-            if(currPoint.equals(stop)) return currCost;
-
-            if(currCost > costMap.get(currPoint)) continue;
-
-            for(int i = 0; i < 4; i++)
-            {
-                Point np = new Point(currPoint.x + dx[i], currPoint.y + dy[i]);
-
-                if(isValid(np) == false) continue;
-
-                int newCost = heuristics(np, stop) + currCost + 1;
-                
-                if(!costMap.containsKey(np) || costMap.get(np) > newCost)
-                {
-                    costMap.put(np, currCost + 1);
-                    pq.add(new Node(np, newCost));
-                }
-
-            }
-            
-        }
-
-        return -1;
-    }
-
-    public static void main(String[] args)
-    {
-        GridSearch gs = new GridSearch("maze1.txt", 81, 81);
-        
-        Scanner scan = new Scanner(System.in);
-        
-        int x1 = scan.nextInt(); int y1 = scan.nextInt();
-        int x2 = scan.nextInt(); int y2 = scan.nextInt();
-
-        scan.close();
-
-        int resInformed = gs.aStart(new Point(x1, y1), new Point(x2, y2));
-        int resUninformed = gs.ucs(new Point(x1, y1), new Point(x2, y2));
-
-        System.out.println("Informed:" + resInformed);
-        System.out.println("Uninformed:" + resUninformed);
-
-        if(resInformed != -1)
-        {
-            System.out.println("YES");
-        }
-        else
-        {
-            System.out.println("NO");
-        }
-    }
 }
+
---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        GridSearch gs = new GridSearch("maze1.txt", 81, 81);
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int x1 = scan.nextInt(); int y1 = scan.nextInt();
+        int x2 = scan.nextInt(); int y2 = scan.nextInt();
+
+        scan.close();
+
+        int resInformed = gs.aStar(new Point(x1, y1), new Point(x2, y2));
+        int resUninformed = gs.ucs(new Point(x1, y1), new Point(x2, y2));
+
+        System.out.println("Informed:" + resInformed);
+        System.out.println("Uninformed:" + resUninformed);
+
+        if(resInformed != -1)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
